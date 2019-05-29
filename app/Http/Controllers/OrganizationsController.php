42fd5fe391dce613eea9df2cce232d4dc962b431
Stanislav Chistenko <skvest1@gmail.com>
2019-05-29 13:51:34 +0600

    Added draft for OrganizationsController

---
+++ app/Http/Controllers/OrganizationsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrganizationsController extends Controller
+{
+    /**
+     * @param $name
+     * @return array
+     */
+    public function show($name)
+    {
+        return [];
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function store(Request $request)
+    {
+        return [];
+    }
+
+}
