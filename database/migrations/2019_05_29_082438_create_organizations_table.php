9ded59d44694a984c728722830a7d13f04c9625b
Stanislav Chistenko <skvest1@gmail.com>
2019-05-29 14:29:11 +0600

    Added Organization model and migration draft with nested set structure

---
+++ database/migrations/2019_05_29_082438_create_organizations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('parent_id');
+            $table->bigInteger('root_id');
+            $table->integer('left');
+            $table->integer('right');
+            $table->integer('level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
