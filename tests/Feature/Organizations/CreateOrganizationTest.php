504e8f0c7ceb208ef31d1663d30741279821533c
Stanislav Chistenko <skvest1@gmail.com>
2019-05-30 10:54:34 +0600

    Added create_organization_successfully test

--- tests/Feature/Organizations/CreateOrganizationTest.php
+++ tests/Feature/Organizations/CreateOrganizationTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CreateOrganizationTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * @test
      */
@@ -29,5 +31,51 @@ class CreateOrganizationTest extends TestCase
 
         $response = $this->json('POST', 'api/organizations', ['org_name' => 'test', 'daughters' => 'test'], []);
         $response->assertStatus(422);
+
+        // Test duplicates
+        $response = $this->json('POST', 'api/organizations', ['org_name' => 'test'], []);
+        $response->assertStatus(200);
+        $response = $this->json('POST', 'api/organizations', ['org_name' => 'test'], []);
+        $response->assertStatus(422);
+    }
+
+    /**
+     * @test
+     */
+    public function create_organization_successfully()
+    {
+        $json = '{
+            "org_name": "Paradise island",
+            "daughters": [
+                {
+                    "org_name": "Banana Tree",
+                    "daughters": [
+                        {"org_name": "Yellow Banana"},
+                        {"org_name": "Brown Banana"},
+                        {"org_name": "Black Banana"}
+                    ]
+                },
+                {
+                    "org_name": "Big Banana Tree",
+                    "daughters": [
+                        {"org_name": "Yellow Banana"},
+                        {"org_name": "Brown Banana"},
+                        {"org_name": "Green Banana"},
+                        {
+                            "org_name": "Black banana",
+                            "daughters": [{
+                                "org_name": "Phoneutria Spider"
+                            }]
+                        }
+                    ]	
+                }
+            ]
+        }';
+        $response = $this->json('POST', 'api/organizations', json_decode($json, true), []);
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+            'message' => trans('organization.created.success'),
+        ]);
     }
 }
